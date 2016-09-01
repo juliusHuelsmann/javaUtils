e0b31514ec2c8b52bcc0768ccaa22d1916a63c6e
Julius Huelsmann <jhuelsmann@techfak.uni-bielefeld.de>
2016-09-01 11:24:27 +0200

    .

--- src/main/java/opaqueFrame/OpFrame.java
+++ src/main/java/opaqueFrame/OpFrame.java
@@ -8,18 +8,27 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.*;
 
+import movelistener.control.Movelistener;
+import utils.Utils;
 import static java.awt.GraphicsDevice.WindowTranslucency.*;
 
 public class OpFrame extends JFrame {
   
-  //TODO: einen eigenen gradientPaint schreiben.
-    public OpFrame() {
+  public OpFrame() {
+    this(true);
+  }
+  public OpFrame(final boolean addMoveListener) {
       super();
+
+      if (addMoveListener) {
+    Movelistener ml = new Movelistener(this);
+    super.addMouseMotionListener(ml);
+    super.addMouseListener(ml);
+      }
       super.setTitle("hier");
       super.setUndecorated(true);
       setBackground(new Color(0,0,0,0));
-      setLocationRelativeTo(null);
-      super.setSize(100,100);
+      super.setSize(350, 450);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel() {
@@ -42,8 +51,8 @@ public class OpFrame extends JFrame {
         setContentPane(panel);
         setLayout(null);
         
-        System.out.println("new version");
-        boolean test = true;
+        setSize(500, 500);
+        boolean test = false;
         if (test) {
           BufferedImage bi = new BufferedImage(50, 10, BufferedImage.TYPE_INT_ARGB);
           for (int i = 0; i < bi.getWidth(); i++) {
@@ -55,11 +64,17 @@ public class OpFrame extends JFrame {
             bi.setRGB(j, j, new Color(50, 50, 50, 150).getRGB());
           }
           JLabel jbtn = new JLabel();
-          jbtn.setSize(50, 10);
+          jbtn.setSize(getSize());
           jbtn.setBorder(null);
           jbtn.setIcon(new ImageIcon(bi));
           jbtn.setOpaque(false);
           super.add(jbtn);
+          
+          BufferedImage biWindowShape = preprocessBackground("/bgShape.png",
+              getSize(), new Rectangle(4, 4, getWidth() - 9, getHeight() - 9), null);
+
+          jbtn.setIcon(new ImageIcon(biWindowShape));
+        
         }
     }
 
@@ -91,4 +106,42 @@ public class OpFrame extends JFrame {
             }
         });
     }
+
+
+    
+    protected static BufferedImage preprocessBackground(
+        final String xpath, final Dimension xdim, final 
+        Rectangle xNotProcess, BufferedImage biWindowShapeInverted) {
+      
+      // load image as RGBA bufferedImage
+      BufferedImage processedImg = Utils.resize(xpath, xdim.width, xdim.height);
+
+      
+      for (int x = 0; x < processedImg.getWidth(); x++) {
+        for (int y = 0; y < processedImg.getHeight(); y++) {
+
+          final Color c = new Color(processedImg.getRGB(x, y));
+          if (xNotProcess == null || !(x >= xNotProcess.x && y >= xNotProcess.y 
+              && x <= xNotProcess.x + xNotProcess.width 
+              && y <= xNotProcess.y + xNotProcess.height)) {
+            
+            int r = Math.max(0, -80 + c.getRed());
+            int g = Math.max(0, -90 + c.getGreen());
+            int b = Math.max(0, -120 + c.getBlue());
+            final int sum = (r + g + b);
+            final int alpha = Math.min(255, Math.max(0, 255 - sum * 255 / (135 + 165 + 175)));
+            processedImg.setRGB(x, y, new Color(80, 90, 120, alpha).getRGB());
+            processedImg.setRGB(x, y, new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha).getRGB());
+          } 
+          
+          if (biWindowShapeInverted != null) {
+            biWindowShapeInverted.setRGB(processedImg.getWidth() - x - 1,
+                processedImg.getHeight() - y - 1, processedImg.getRGB(x, y));
+          }
+        }
+      }
+      return processedImg;
+    }
+    
+
 }
